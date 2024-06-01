0ffcbae200d3faabb0e8d0e0a12ba21caafc6d7c
Samir6467ff <kariokie01@gmail.com>
2024-06-01 14:02:17 +0300

    Update الجرس2.js

--- plugins/الجرس2.js
+++ plugins/الجرس2.js
@@ -23,5 +23,5 @@ let love = `
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['estupidez']
 handler.tags = ['fun']
-handler.command = /^(الجرس|جرس)$/i
+handler.command = /^(الجرس|جرس2)$/i
 export default handler
