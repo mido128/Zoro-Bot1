1ab24554b73a1d5b229ea927b6b172621c755eec
Samir6467ff <kariokie01@gmail.com>
2024-06-01 13:48:42 +0300

    Update قلوب3.js

--- plugins/قلوب3.js
+++ plugins/قلوب3.js
@@ -16,5 +16,5 @@ let love = `
 m.reply(love, null, { mentions: conn.parseMention(love) })}
 handler.help = ['estupidez']
 handler.tags = ['fun']
-handler.command = /^(القلوب|قلوب)$/i
+handler.command = /^(القلوب|قلوب3)$/i
 export default handler
